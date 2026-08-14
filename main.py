a407a6c7e31ad14fd79ba3b920258bc2ebdc5e45
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2026-08-14 15:12:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+from dotenv import load_dotenv
 
+from trends.collector import (
+    collect_trends
+)
+
+from trends.storage import (
+    save_trends
+)
+
+
+def main():
+
+    print(
+        "\n"
+        "🚀 YouTube Global Shorts"
+        "\n"
+    )
+
+    trends = collect_trends()
+
+    save_trends(
+        trends
+    )
+
+    print(
+        "\n"
+        "✅ Trend Engine завершён"
+        "\n"
+    )
+
+
+if __name__ == "__main__":
+    main()
